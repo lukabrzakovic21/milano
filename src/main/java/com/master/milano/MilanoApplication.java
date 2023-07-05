843dc13848678ce38a44f403fe99f75d8b5cfae5
Luka Brzakovic <luka.brzakovic@sbgenomics.com>
2023-07-05 14:00:50 +0200

    Barebone service

---
+++ src/main/java/com/master/milano/MilanoApplication.java
@@ -0,0 +1,13 @@
+package com.master.milano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/master/milano/MilanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.milano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
